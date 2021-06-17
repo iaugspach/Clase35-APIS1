35a14782ecf2d8515347ce8d14d0d75e44cf9b40
iaugspach <iaugspach97@gmail.com>
2021-06-17 17:23:28 -0300

    isa

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const indexRouter = require('./routes/index');
 const moviesRoutes = require('./routes/moviesRoutes');
 const genresRoutes = require('./routes/genresRoutes');
 
-//Aquí pueden colocar las rutas de las APIs
+//Aquí llamo a la ruta de las api de movies
+const apiMoviesRouter = require('./routes/api/movies')
+//Aquí llamo a la ruta de las api de actors
+const apiGenresRouter = require('./routes/api/genres')
+//Aquí llamo a la ruta de las api de actors
+const apiActorsRouter = require('./routes/api/actors')
 
 
 // view engine setup
@@ -27,6 +32,10 @@ app.use(methodOverride('_method'));
 app.use('/', indexRouter);
 app.use(moviesRoutes);
 app.use(genresRoutes);
+//Aquí creo la colección de mis recursos de movies (APIs)
+app.use('/api/movies',apiMoviesRouter);
+app.use('/api/actors',apiActorsRouter);
+app.use('/api/genres',apiGenresRouter);
 
 
 //Activando el servidor desde express
